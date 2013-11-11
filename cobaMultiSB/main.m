2ad0d8daca67a38f2c82369c499740d657fde7c1
Dody <dodyrw@gmail.com>
2013-11-11 13:38:09 +0700

    Initial Commit

---
+++ cobaMultiSB/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  cobaMultiSB
+//
+//  Created by Dody Rachmat Wicaksono on 11/11/13.
+//  Copyright (c) 2013 Dody Rachmat Wicaksono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
